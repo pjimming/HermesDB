789abae8b35b41198a2a0482fd138ab026eb4685
pan <2556323541@qq.com>
2023-11-21 22:28:56 +0800

    commit

--- cachemanager/cachemanager.go
+++ cachemanager/cachemanager.go
@@ -26,15 +26,29 @@ func NewCacheManager(bucketSize, numFrames, k SizeT) *CacheManager {
 func (c *CacheManager) Set(key string, value any) {
 	c.mutex(key).Lock()
 	defer c.mutex(key).Unlock()
+
 	// hash table
 	c.table.Insert(key, value)
 
 	// replacer
 	c.replacer.RecordAccess(key)
+	c.replacer.SetEvictable(key, true)
 }
 
 func (c *CacheManager) Get(key string) any {
-	return nil
+	c.mutex(key).RLock()
+	defer c.mutex(key).RUnlock()
+
+	// hash table
+	value, isFind := c.table.Find(key)
+	if !isFind {
+		return nil
+	}
+
+	// replacer
+	c.replacer.RecordAccess(key)
+
+	return value
 }
 
 func (c *CacheManager) mutex(key string) *sync.RWMutex {
